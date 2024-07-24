c635f11a7c6e4bea16364ce018d036899c8aacc8
koushikreddybachu <koushi.bachu@gmail.com>
2024-07-24 20:47:45 +0530

    added new program

---
+++ Exercise-1/StudentMark.java
@@ -0,0 +1,11 @@
+class StudentMark
+{
+    public static void main(String a[])
+    {
+        System.out.println("Roll NO:"+a[0]);
+        System.out.println("Name:"+a[1]);
+        System.out.println("Subject 1-"+a[2]);
+        System.out.println("Subject 2-"+a[3]);
+        System.out.println("Subject 3-"+a[4]);
+    }
+}
